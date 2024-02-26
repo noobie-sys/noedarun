1cf78cc8e4386ac69e2c13d8c8ab52835854817a
Aman Gupta <amangupta954055@gmail.com>
2024-02-26 19:16:06 +0530

    made responsive product details

--- components/singleProductPage/ProductDetailsPage/page.tsx
+++ components/singleProductPage/ProductDetailsPage/page.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { AccordionProductDetails } from "../AccordianDetails";
 import Image from "next/image";
 import { urlFor } from "@/app/lib/sanity";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
 interface Props {
   images: Array<string>;
@@ -11,38 +13,75 @@ interface Props {
 const ProductDetails = ({ images, descriptions }: Props) => {
   const desc1 = descriptions.slice(0, 448);
   const desc2 = descriptions.slice(448);
-//   console.log("------------", images, "hello there");
+  //   console.log("------------", images, "hello there");
   return (
-    <div className="w-full h-screen ">
+    <div className="w-full min-h-screen ">
       <div className="w-full h-full flex flex-col lg:flex-row justify-between  px-5">
-        <div className="Imagegallery lg:w-1/2 h-fit flex flex-wrap gap-1">
-          { 
-            images.map((el, i ) => (
-              <div
-                key={i}
-                className="w-[22vw] h-[32vh] bg-[#F4F4F4] overflow-hidden cursor-pointer"
-              >
-                <div className="w-full h-full overflow-hidden">
-                  <Image
-                    src={urlFor(el).url()}
-                    alt="Shoes Detials"
-                    width={500}
-                    height={500}
-                    className="w-full h-full object-contain object-center hover:scale-105 transition-all 300ms ease-linear"
-                  />
-                </div>
+        <div className="hidden Imagegallery lg:w-1/2 h-fit lg:flex flex-wrap gap-1">
+          {images.map((el, i) => (
+            <div
+              key={i}
+              className="w-[22vw] h-[32vh] bg-[#F4F4F4] overflow-hidden cursor-pointer"
+            >
+              <div className="w-full h-full overflow-hidden">
+                <Image
+                  src={urlFor(el).url()}
+                  alt="Shoes Detials"
+                  width={500}
+                  height={500}
+                  priority
+                  className="w-full h-full object-contain object-center hover:scale-105 transition-all 300ms ease-linear"
+                />
               </div>
-            ))}
+            </div>
+          ))}
         </div>
-        <div className="w-[50vw]">
-          <div className="w-full">
-            <p className="text-2xl text-gray-500">{desc1}</p>
-            <p className="text-2xl text-gray-500 mt-10">{desc2}</p>
+
+        <div className="lg:hidden">
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={20}
+            effect={"coverflow"}
+            grabCursor={true}
+            loop={true}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className="mySwiper"
+           
+          >
+            {images && images.map((el, i) => (
+              <SwiperSlide key={i} className=" overflow-hidden  lg:w-[50vw] w-full h-full bg-[#F4F4F4]">
+                <div
+                  key={i}
+                  className="w-full h-full bg-[#F4F4F4] overflow-hidden cursor-pointer"
+                >
+                  <div className="w-full h-full overflow-hidden">
+                    <Image
+                      src={urlFor(el).url()}
+                      alt="Shoes Detials"
+                      width={500}
+                      height={500}
+                      priority
+                      className="w-full h-full object-contain object-center hover:scale-105 transition-all 300ms ease-linear"
+                    />
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className="lg:w-[50vw] h-full">
+          <div className="w-full mt-10 lg:mt-0">
+            <p className="lg:text-2xl text-gray-500">{desc1}</p>
+            <p className="lg:text-2xl text-gray-500 mt-10">{desc2}</p>
             <p className="text-2xl mt-10 text-gray-500">
               Designed to prove that true sustainability is found in durability.
             </p>
           </div>
-          <div className="mt-32">
+          <div className="lg:mt-32 mt-10 mb-20">
             <AccordionProductDetails />
           </div>
         </div>
--- components/singleProductPage/gallerySlider/index.tsx
+++ components/singleProductPage/gallerySlider/index.tsx
@@ -18,23 +18,42 @@ interface Props {
 export default function SliderGallery({ imageGallery }: Props) {
   // console.log(imageGallery);
 
+
+
+  if(!imageGallery) return;
+
   return (
-    <div className="w-full h-screen  bg-black relative">
-        <h1 className="text-white absolute top-10 left-20">Gallery</h1>
+    <div className="w-full lg:h-screen h-[60vh]  bg-black relative">
+        <h1 className="text-white absolute top-20 lg:top-10 left-1 lg:left-20">Gallery</h1>
       {/* </div> */}
 
-      <div className="w-full h-full p-20 ">
+      <div className="w-full h-full lg:p-20 flex justify-center items-center ">
         <Swiper
           slidesPerView={2}
           spaceBetween={20}
-          scrollbar={{ draggable: true, hide: true }}
-          modules={[Scrollbar, Navigation]}
-          className="mySwiper w-full h-full"
+        //   scrollbar={{ draggable: true, hide: true , dragClass: "swiper-scrollbar-drag" }}
+        //   pagination={{ clickable: true ,  dynamicBullets: true }}
+          modules={[ Pagination]}
+          className="mySwiper"
           grabCursor={true}
           loop={false}
+          breakpoints={{
+            0: {
+              slidesPerView: 1,
+              spaceBetween: 10,
+            },
+            768: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 2,
+              spaceBetween: 40,
+            },
+          }}
         >
-          {imageGallery.map((image, i) => (
-            <SwiperSlide key={i} className="w-full h-full ">
+          {imageGallery && imageGallery.map((image, i) => (
+            <SwiperSlide key={i} className="lg:w-full lg:h-full w-[50vw] h-[40vw] ">
               <div className="w-full h-full overflow-hidden">
                 <Image
                   src={urlFor(image).url()}
@@ -47,12 +66,7 @@ export default function SliderGallery({ imageGallery }: Props) {
               </div>
             </SwiperSlide>
           ))}
-          {/* <SwiperSlide className="bg-green-500">
-            <div className="">
-                slide 1
-            </div>
-        </SwiperSlide>
-        <SwiperSlide className="bg-red-500 w-[40vw]">Slide 2</SwiperSlide> */}
+          
         </Swiper>
       </div>
     </div>
