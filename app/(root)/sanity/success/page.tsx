6dcc0655dc637623f7383f391765552da64780f8
Aman Gupta <amangupta954055@gmail.com>
2024-02-27 15:54:00 +0530

    add stripe sucess page

---
+++ app/(root)/sanity/success/page.tsx
@@ -0,0 +1,11 @@
+import { Divide } from "lucide-react";
+
+const Success = () => {
+  return (
+    <div className="success-page">
+      <h1>Successi</h1>
+    </div>
+  );
+};
+
+export default Success;
