5be40d6bf8be5997d6a2a2f7ea825690fffbd6cb
Aman Gupta <amangupta954055@gmail.com>
2024-02-28 23:46:35 +0530

    bug fixes

--- components/lottie/empty.tsx
+++ components/lottie/empty.tsx
@@ -15,9 +15,9 @@ const Empty = () => {
 
         style={{width : "600px", height : "600px"}}
       /> */}
-      <div className="w-full flex justify-center text-xl text-gray-500 uppercase">
+      <div className="w-full flex flex-col items-center justify-center text-xl text-gray-500 uppercase">
         <p>
-          We don&apos;t have in our <span className="text-red-500">Norda</span>yet
+          We don&apos;t have in our <span className="text-red-500">Norda</span> yet
         </p>
         <Link href="/collections/all-products/" className="text-red-500 ml-2">Shop Now</Link>
       </div>
